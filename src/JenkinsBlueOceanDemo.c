ef2f3b2141e04591a6105c0f5186f64951017232
S-OConnor <35660008+S-OConnor@users.noreply.github.com>
2018-11-10 21:06:13 -0500

    Update to executing
    Update to executing

--- src/JenkinsBlueOceanDemo.c
+++ src/JenkinsBlueOceanDemo.c
@@ -1,4 +1,4 @@
-
+//Libaries
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,61 +10,93 @@
 #include <assert.h>
 #include <sys/types.h>
 #include <string.h>
+#include <tchar.h>
+#include <windows.h>
 
-#define _PATH "PATH"
-#define _PATH_CURRENT_DIR "./"
-#define _PATH_SEP ":"
+//Constants
 #define VERBOSE_OPT "-Verbose"
 #define DEBUG_OPT "-Debug"
 #define ADD_OPT "-Add"
 #define MUL_OPT "-Mul"
+#define	BUFSIZE 256
 
+
+//Global Variables
 static unsigned char AddFlag = 0;
 static unsigned char MulFlag = 0;
+static const char delimiters[4] = "  \t\n";
 
+//Main Function
 int main(int argc, char** argv) {
-	char* FileName_String;
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-
 	//Parse Command Line Args
+	char* FileName_String;
 	for (int i = 1; i < argc; i++)
 	{
 		if(0 == strcmp(ADD_OPT, argv[i]))AddFlag =1;
 	    else if(0 == strcmp(MUL_OPT, argv[i]))MulFlag = 1;
 	    else {
-	    	fprintf(stderr, "Command line option: %s\n", argv[i]);
+	    	printf("Command line option: %s\n", argv[i]);
 	    	FileName_String = argv[i];
 		}
 	}
-	fprintf(stderr, "File name from command argument: %s\n", FileName_String);
-	char * path = getenv(_PATH);
-	int Buffer_Length;
-	Buffer_Length = strlen(_PATH_CURRENT_DIR) + strlen(_PATH_SEP) + strlen(path) + 1;
-	char buf[Buffer_Length];
-	char buf2[strlen(_PATH_CURRENT_DIR) + strlen(_PATH_SEP) + strlen(path) + 1];
 
-	strcpy(buf,_PATH_CURRENT_DIR);
-	strcat(buf,_PATH_SEP);
-	strcat(buf,path);
-	fprintf(stderr, "path=%s\n",buf);
+	//Get current working Directory
+	TCHAR Buffer[256];
+	GetCurrentDirectory(BUFSIZE, Buffer);
+	printf("Current Working Directory: %s \n", Buffer);
+	strcat(Buffer,"\\");
+	strcat(Buffer,FileName_String);
+	printf("File name and path: %s \n", Buffer);
 
-	setenv(_PATH, buf, 1);
-	fprintf(stderr, "path=%s\n", getenv(_PATH));
-
-
-/*
+	//Find and open file
 	FILE* FP;
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    FP = fopen("/etc/motd", "r");
-    if (FP == NULL)
+    char line[256];
+    FP = fopen(Buffer, "r");
+    if (FP == NULL) {
+    	printf("File Pointer is set to NULL!!!!");
         exit(EXIT_FAILURE);
-    while ((read = getline(&line, &len, FP)) != -1) {
-        printf("Retrieved line of length %zu :\n", read);
-        printf("%s", line);
+    }
+
+    //Add Function
+    if(AddFlag==1){
+        int linenumber = 1;
+        int num1, num2, ans;
+        while (fgets(line, 256, FP) != NULL){
+        	printf("%i: ", linenumber);
+			printf("Retrieved line: %s", line);
+			linenumber++;
+        	num1 = atoi(strtok(line, delimiters));
+        	num2 = atoi(strtok(NULL, delimiters));
+        	ans = atoi(strtok(NULL, delimiters));
+        	if(num1 + num2 != ans){
+        		printf( "    Failed Test %i + %i != %i \n", num1, num2, ans);
+        	}
+        }
+    }
+
+    //Square Function
+    else if(MulFlag==1){
+    	int linenumber = 1;
+			int num1, ans;
+			while (fgets(line, 256, FP) != NULL){
+				printf("%i: ", linenumber);
+				printf("Retrieved line: %s", line);
+				linenumber++;
+				num1 = atoi(strtok(line, delimiters));
+				ans = atoi(strtok(NULL, delimiters));
+				if(num1 * num1 != ans){
+					printf( "    Failed Test %i + %i != %i \n", num1, num1, ans);
+				}
+			}
+    }
+    else {
+    	fprintf(stderr, "No Flag Set!!!");
+    	fclose(FP);
+    	exit(EXIT_SUCCESS);
     }
     fclose(FP);
-*/
+
 	return EXIT_SUCCESS;
 }
+
+
